a4b1217808fd3b58854156193658a27293b50fbb
Wang, Hedi <wanghedi1991@gmail.com>
2015-09-27 06:57:33 -0400

    add event layout

--- app/src/main/java/hackgt/crowder/fragment/AddEventDialogFragment.java
+++ app/src/main/java/hackgt/crowder/fragment/AddEventDialogFragment.java
@@ -1,16 +1,25 @@
 package hackgt.crowder.fragment;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.hackgt.R;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by hediwang on 15/9/26.
@@ -21,6 +30,72 @@ public class AddEventDialogFragment extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         final View view = inflater.inflate(R.layout.dialog_add_event, null);
+        final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        final DateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+
+        final EditText startDate = ((EditText) view.findViewById(R.id.startDate));
+
+        final EditText startTime = ((EditText) view.findViewById(R.id.startTime));
+        final EditText endDate = ((EditText) view.findViewById(R.id.endDate));
+        final EditText endTime = ((EditText) view.findViewById(R.id.endTime));
+        startDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog dialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        Date date = new Date(year - 1900, monthOfYear, dayOfMonth);
+                        startDate.setText(dateFormat.format(date));
+                    }
+                }, 2015, 10, 01);
+                dialog.show();
+            }
+        });
+        endDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog dialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        Date date = new Date(year - 1900, monthOfYear, dayOfMonth);
+                        endDate.setText(dateFormat.format(date));
+                    }
+                }, 2015, 10, 03);
+                dialog.show();
+            }
+        });
+        startTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog dialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Date date = new Date();
+                        date.setHours(hourOfDay);
+                        date.setMinutes(minute);
+                        date.setSeconds(0);
+                        startTime.setText(timeFormat.format(date));
+                    }
+                }, 00, 00, true);
+                dialog.show();
+            }
+        });
+        endTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog dialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Date date = new Date();
+                        date.setHours(hourOfDay);
+                        date.setMinutes(minute);
+                        date.setSeconds(0);
+                        endTime.setText(timeFormat.format(date));
+                    }
+                }, 00, 00, true);
+                dialog.show();
+            }
+        });
         builder.setView(view)
                 .setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
                     @Override
@@ -37,7 +112,9 @@ public class AddEventDialogFragment extends DialogFragment {
                 dismiss();
             }
         });
-        return builder.create();
+        Dialog dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(false);
+        return dialog;
     }
 
 }
